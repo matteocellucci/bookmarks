d2b6fb3079224d1725aef054d74f89c74ad91dc2
Matteo Cellucci <matteocellucci@gmail.com>
2019-09-19 08:01:32 +0200

    Version 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='Bookmarks',
-    version='1.0',
+    version='0.1.0',
     py_modules=['bookmarks'],
     install_requires=['click'],
     entry_points='''
